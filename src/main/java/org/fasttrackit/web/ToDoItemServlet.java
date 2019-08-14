78921f0fd43e6bc68656543b8ef2476bda88743b
susana.serban <susana.serban@casomes.ro>
2019-08-14 21:12:42 +0300

    added delete toDoItem endpoint

--- src/main/java/org/fasttrackit/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/web/ToDoItemServlet.java
@@ -56,4 +56,14 @@ public class ToDoItemServlet extends HttpServlet {
 
 
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String id = req.getParameter("id");
+        try {
+            toDoItemService.deleteToDoItem((Long.parseLong(id)));
+        } catch (SQLException | ClassNotFoundException e) {
+              resp.sendError(500,"Internal server error: " + e.getMessage());
+        }
+    }
 }
